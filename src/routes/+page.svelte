eb97878214e0fa936bc8acfdc5e25b24fa6a584e
hmprt <harvey@atomicsear.ch>
2023-06-17 17:06:12 -0500

    Changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class='w-full h-full  bg-black'>
+    <h1 class='text-red-300'>Test</h1>
+
+</div>
